18a4d717de31a1843703c658179cf50765a4bd48
Jeremy Chatfield <jezchatfield@gmail.com>
2012-01-17 09:31:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboNestedModel::Application.config.secret_token = '615daa5d51da6920f05a4b39510ede6d6a4bb6aad52a8fe0e2f9fa3e34911358434ebedfa7b120dd0886ac87546ea0a57a070238f231b629447aa6b8207252fa'
